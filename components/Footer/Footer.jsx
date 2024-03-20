880e11d408a835aff942eb515395dd11d81c447e
nikmel239 <man.nikita0@yandex.ru>
2024-03-20 20:40:05 +0300

    Add layout component Footer

---
+++ components/Footer/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+  return (
+    <footer className={`text-center text-xs border border-t-blue-400 mt-4`}>
+      2024 © All right reserved
+    </footer>
+  );
+}
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,11 +1,12 @@
 import { Header } from "../components/Header";
+import { Footer } from "../components/Footer";
 
 function HomePage() {
   return (
     <div className={`bg-stone-200 min-h-screen`}>
       <Header />
       <div>Main</div>
-      <div>Footer</div>S
+      <Footer />
     </div>
   );
 }
